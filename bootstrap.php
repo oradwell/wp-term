4eb80d75d0c59a38ea9229fa8a573630d80191f5
Omer Karadagli <ufokaradagli@gmail.com>
2014-06-26 20:37:16 +0100

    Simplify autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -2,15 +2,13 @@
 
 spl_autoload_register(function ($className) {
     $srcDir = 'src';
-    $className = ltrim($className, '\\');
-    $fileName  = '';
-    $namespace = '';
+    $fileName  = __DIR__ . DIRECTORY_SEPARATOR . $srcDir . DIRECTORY_SEPARATOR;
     if ($lastNsPos = strrpos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fileName .= $className . '.php';
 
-    require __DIR__ . DIRECTORY_SEPARATOR . $srcDir . DIRECTORY_SEPARATOR . $fileName;
+    require $fileName;
 });
