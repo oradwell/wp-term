4d03dd1e8ca0ffc9efd1ff331aa7870599755243
Omer Karadagli <ufokaradagli@gmail.com>
2014-06-28 22:26:41 +0100

    Add more tests for GotoCommand

--- tests/GotoCommandTest.php
+++ tests/GotoCommandTest.php
@@ -16,4 +16,21 @@ class GotoCommandTest extends PHPUnit_Framework_TestCase
     {
         $this->gotoCommand->execute();
     }
+
+    public function testReturnsNullWhenPostNotFound()
+    {
+        $res = $this->gotoCommand->addArguments('about')
+            ->execute();
+
+        $this->assertNotEmpty($res);
+        $this->assertEquals('http://www.ockwebs.com/about/', $res['url']);
+    }
+
+    public function testReturnsPostUrl()
+    {
+        $res = $this->gotoCommand->addArguments('not-a-post')
+            ->execute();
+
+        $this->assertNull($res);
+    }
 }
