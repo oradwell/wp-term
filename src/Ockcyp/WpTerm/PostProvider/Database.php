203fdecbdfe562ae706a7a68c128f69eef699225
Omer Karadagli <ufokaradagli@gmail.com>
2014-08-05 23:07:10 +0100

    Add PHPDoc to post providers

--- src/Ockcyp/WpTerm/PostProvider/Database.php
+++ src/Ockcyp/WpTerm/PostProvider/Database.php
@@ -6,62 +6,136 @@ use PDO;
 
 class Database extends PostProviderAbstract
 {
+    /**
+     * PDO connection
+     *
+     * @var PDO
+     */
     protected $dbh;
+
+    /**
+     * PDO statement
+     *
+     * @var PDOStatement
+     */
     protected $stmt;
+
+    /**
+     * Current row
+     *
+     * @var array
+     */
     protected $current;
+
+    /**
+     * Row ID
+     *
+     * @var integer
+     */
     protected $key;
 
+    /**
+     * Initialise
+     *
+     * @param PDO $dbh PDO connection
+     */
     public function __construct($dbh)
     {
         $this->dbh = $dbh;
     }
 
+    /**
+     * Get post ID
+     *
+     * @return integer Post ID
+     */
     public function getId()
     {
-        return $this->current[0];
+        return (int) $this->current[0];
     }
 
+    /**
+     * Get post year
+     *
+     * @return integer Year
+     */
     public function getYear()
     {
-        return $this->current[1];
+        return (int) $this->current[1];
     }
 
+    /**
+     * Get post month number
+     *
+     * @return integer Month
+     */
     public function getMonthNum()
     {
-        return $this->current[2];
+        return (int) $this->current[2];
     }
 
+    /**
+     * Get day
+     *
+     * @return integer Day
+     */
     public function getDay()
     {
-        return $this->current[3];
+        return (int) $this->current[3];
     }
 
+    /**
+     * Get post name
+     *
+     * @return string Post name
+     */
     public function getPostName()
     {
         return $this->current[4];
     }
 
+    /**
+     * Get post type post|page
+     *
+     * @return string Post type
+     */
     public function getType()
     {
         return $this->current[5];
     }
 
+    /**
+     * Get current row as Post object while iterating
+     *
+     * @return Post
+     */
     public function current()
     {
         return $this->getPostInstance();
     }
 
+    /**
+     * Iterate to next post
+     */
     public function next()
     {
         $this->key++;
         $this->current = $this->stmt->fetch(PDO::FETCH_NUM);
     }
 
+    /**
+     * Get iteration key
+     *
+     * @return integer Key
+     */
     public function key()
     {
         return $this->key;
     }
 
+    /**
+     * Move to beginning of the iteration
+     */
     public function rewind()
     {
         if (!$this->dbh) {
@@ -78,6 +152,11 @@ class Database extends PostProviderAbstract
         $this->current = $this->stmt->fetch(PDO::FETCH_NUM);
     }
 
+    /**
+     * Check if current iteration is valid
+     *
+     * @return boolean True if iteration is valid
+     */
     public function valid()
     {
         return (bool) $this->current;
--- src/Ockcyp/WpTerm/PostProvider/File.php
+++ src/Ockcyp/WpTerm/PostProvider/File.php
@@ -4,61 +4,129 @@ namespace Ockcyp\WpTerm\PostProvider;
 
 class File extends PostProviderAbstract
 {
+    /**
+     * File handle
+     *
+     * @var integer
+     */
     protected $fh;
+
+    /**
+     * Current row
+     *
+     * @var array
+     */
     protected $current;
+
+    /**
+     * Row ID
+     *
+     * @var integer
+     */
     protected $key;
 
+    /**
+     * Initialise
+     *
+     * @param $fh File handle
+     */
     public function __construct($fh)
     {
         $this->fh = $fh;
     }
 
+    /**
+     * Get post ID
+     *
+     * @return integer Post ID
+     */
     public function getId()
     {
-        return $this->current[0];
+        return (int) $this->current[0];
     }
 
+    /**
+     * Get post year
+     *
+     * @return integer Year
+     */
     public function getYear()
     {
-        return $this->current[1];
+        return (int) $this->current[1];
     }
 
+    /**
+     * Get post month number
+     *
+     * @return integer Month
+     */
     public function getMonthNum()
     {
-        return $this->current[2];
+        return (int) $this->current[2];
     }
 
+    /**
+     * Get day
+     *
+     * @return integer Day
+     */
     public function getDay()
     {
-        return $this->current[3];
+        return (int) $this->current[3];
     }
 
+    /**
+     * Get post name
+     *
+     * @return string Post name
+     */
     public function getPostName()
     {
         return $this->current[4];
     }
 
+    /**
+     * Get post type post|page
+     *
+     * @return string Post type
+     */
     public function getType()
     {
         return $this->current[5];
     }
 
+    /**
+     * Get current row as Post object while iterating
+     *
+     * @return Post
+     */
     public function current()
     {
         return $this->getPostInstance();
     }
 
+    /**
+     * Iterate to next post
+     */
     public function next()
     {
         $this->key++;
         $this->current = fgetcsv($this->fh);
     }
 
+    /**
+     * Get iteration key
+     *
+     * @return integer Key
+     */
     public function key()
     {
         return $this->key;
     }
 
+    /**
+     * Move to beginning of the iteration
+     */
     public function rewind()
     {
         if (!$this->fh) {
@@ -70,6 +138,11 @@ class File extends PostProviderAbstract
         $this->current = fgetcsv($this->fh);
     }
 
+    /**
+     * Check if current iteration is valid
+     *
+     * @return boolean True if iteration is valid
+     */
     public function valid()
     {
         return (bool) $this->current;
